1c4c32d79f1c26ccc7a4da39b8f6a56fbc97bf6d
mauricio <maur28363429rd@gmail.com>
2023-11-29 16:57:35 -0400

    sigma comand

---
+++ prueba-rendimiento.js
@@ -0,0 +1,9 @@
+for (let i = 0; i < 1000; i++) {
+  fetch('https://devsigma.mauricioreyesdev.com/')
+    .then(response => {
+      // Manejar la respuesta si es necesario
+    })
+    .catch(error => {
+      // Manejar el error si ocurre
+    });
+}
